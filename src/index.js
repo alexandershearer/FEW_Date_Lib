607482ddcb2ec43b95612501a4728aacb004134c
alexandershearer <alex.shearer23@gmail.com>
2021-02-08 20:01:51 -0600

    Finishing up testing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
+const { months, days } = require('./util')
+
 class D {
     constructor(...args) {
         this._date = new Date(...args)
-        this.months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-        this.days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-
     }
     get year() {
         return this._date.getFullYear()
@@ -12,16 +11,16 @@ class D {
         return this._date.getFullYear().toString().substr(2, 3)
     }
     get month() {
-        return this.months[this._date.getMonth()]
+        return months[this._date.getMonth()]
     }
     get m() {
-        return this.months[this._date.getMonth()].toString().substr(0, 3)
+        return months[this._date.getMonth()].toString().substr(0, 3)
     }
     get day() {
-        return this.days[this._date.getDay()]
+        return days[this._date.getDay()]
     }
     get dy() {
-        return this.days[this._date.getDay()].toString().substr(0, 3)
+        return days[this._date.getDay()].toString().substr(0, 3)
     }
     get date() {
         return this._date.getDate()
@@ -129,36 +128,10 @@ class D {
             return 'Today'
         }
 
-
     }
 
 }
 
-// With no parameters: 
-const a = new D()
-// Create a date from a string (single parameter)
-const b = new D('9/26/1965')
-// Create a date from some numbers (list of parameters)
-const c = new D(1970, 1, 1, 0, 0, 0)
-// With a Date
-const d = new D(new Date())
+var test = 'Test String'
 
-// console.log(a.year)
-// console.log(a.yr)
-// console.log(a.month)
-// console.log(a.m)
-// console.log(a.day)
-// console.log(a.dy)
-// console.log(a.date)
-// console.log(a.hours)
-// console.log(a.mins)
-// console.log(a.secs)
-
-
-console.log(a.formatDate('y/m/d'))
-
-console.log(a.formatDate('H:I:S'))
-
-console.log(a.formatDate('Y-M-D h:i:s'))
-
-console.log(b.when())
+module.exports = D
